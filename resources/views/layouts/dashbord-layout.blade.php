04f430aa5386693752e54ebc7b1db694ed452814
fly23java <himaishere216@gmail.com>
2024-03-16 20:36:09 +0300

    fix show logout in mobile screen and show menu

--- resources/views/layouts/dashbord-layout.blade.php
+++ resources/views/layouts/dashbord-layout.blade.php
@@ -186,10 +186,10 @@
     <nav class="header-navbar navbar navbar-expand-lg align-items-center floating-nav navbar-light navbar-shadow">
         <div class="navbar-container d-flex content">
             <div class="bookmark-wrapper d-flex align-items-center">
-                <!-- <ul class="nav navbar-nav d-xl-none">
+                <ul class="nav navbar-nav d-xl-none">
                     <li class="nav-item"><a class="nav-link menu-toggle" href="javascript:void(0);"><i class="ficon" data-feather="menu"></i></a></li>
                 </ul>
-                <ul class="nav navbar-nav bookmark-icons">
+                <!-- <ul class="nav navbar-nav bookmark-icons">
                     <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-email.html" data-toggle="tooltip" data-placement="top" title="Email"><i class="ficon" data-feather="mail"></i></a></li>
                     <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-chat.html" data-toggle="tooltip" data-placement="top" title="Chat"><i class="ficon" data-feather="message-square"></i></a></li>
                     <li class="nav-item d-none d-lg-block"><a class="nav-link" href="app-calendar.html" data-toggle="tooltip" data-placement="top" title="Calendar"><i class="ficon" data-feather="calendar"></i></a></li>
@@ -384,11 +384,27 @@
                     </ul>
                 </li> -->
                 <li class="nav-item dropdown dropdown-user">
-                    <a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="javascript:void(0);" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <div class="user-nav d-sm-flex d-none"><span class="user-name font-weight-bolder">
-                        {{ Auth::user()->name }} 
-                    
-                    </a>
+                <a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="javascript:void(0);" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <div class="user-nav d-sm-flex d-none ">
+                            <span class="user-name font-weight-bolder">{{ Auth::user()->name }} </span>
+                            <span class="user-status">
+                          
+                            </span>
+                        </div>
+                        <span class="avatar">
+                        {{ Auth::user()->Account->name_arabic }} 
+                          
+                        </span>
+                </a>    
+
+
+                    <!-- <a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="javascript:void(0);" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <div class="user-nav d-sm-flex d-none">
+                            <span class="user-name font-weight-bolder">
+                                {{ Auth::user()->name }} 
+                            </span>
+                        </div>
+                    </a> -->
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown-user">
                         <!-- <a class="dropdown-item" href="page-profile.html">
                             <i class="mr-50" data-feather="user"></i>
