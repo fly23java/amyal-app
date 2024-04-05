33271df5a81f06220a868625315d09ce7419ce64
fly23java <himaishere216@gmail.com>
2024-04-05 22:33:05 +0300

    fix

--- config/app.php
+++ config/app.php
@@ -163,8 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Yajra\DataTables\DataTablesServiceProvider::class,
-
+       
         /*
          * Package Service Providers...
          */
@@ -196,7 +195,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // ...
 
-        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        
        
     ])->toArray(),
 
